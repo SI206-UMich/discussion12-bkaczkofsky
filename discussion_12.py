0ede0b1a345a2e56fb0a50dc1c70e1fc496afeea
Brogan Kaczkofsky <brogan@umich.edu>
2022-11-30 13:51:01 -0500

    finished task 3

--- discussion_12.py
+++ discussion_12.py
@@ -57,6 +57,11 @@ def job_and_hire_date(cur, conn):
 # TASK 3: IDENTIFY PROBLEMATIC SALARY DATA
 # Apply JOIN clause to match individual employees
 def problematic_salary(cur, conn):
+    cur.execute('SELECT employees.first_name, employees.last_name FROM employees JOIN jobs ON employees.job_id = jobs.job_id WHERE employees.salary > jobs.max_salary OR employees.salary < jobs.min_salary')
+    invalid = cur.fetchall()
+    conn.commit()
+    # print(invalid)
+    return invalid
     pass
 
 # TASK 4: VISUALIZATION
